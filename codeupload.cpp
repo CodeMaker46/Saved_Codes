f8cd762b24f8e596a1680b63cc1223082f0228b1
Shikshak Kumar <138814848+CodeMaker46@users.noreply.github.com>
2024-05-13 11:57:43 +0530

    Update codeupload.cpp

--- codeupload.cpp
+++ codeupload.cpp
@@ -7,7 +7,7 @@
 #define fd(i,n,x) for(ll i=n;i>=x;i--)
 #define pb push_back
 // Formula : a*b = lcm(a,b) * gcd(a,b)
-// Formual to make pairs : (n*(n-1) )/2
+// Formula to make pairs : (n*(n-1) )/2
 // lower_bound(v.begin(),v.end(),L) gives you the pointer of the value greater than or equal to L
 // lower_bound(v.begin(),v.end(),L) - v.begin() gives you the index of the value greater than or equal to L
 // sort(v.begin(),v.end(),greater<ll>()); sorts the array in descendind order
@@ -19,12 +19,8 @@
 // LLONG_MAX is for long long (usually 64-bit signed integer).
 
 using namespace std;
-ll lcm(ll a, ll b){a=(a*b)/gcd(a,b); return a;} 
-ll gcd(ll a, ll b){if(b==0) {return a;} return gcd(b,a%b);}
-ll prime(ll n){ if(n==1 || n==0) return 0; for(ll i=2;i*i<=n;i++) if(n%i==0) return 0;return 1;}
-ll power(ll x, ll n){ll res=1;while(n){if(n&1) res*=x,n--;else x*=x,n/=2;}return res;}
-ll ncr(ll n, ll r){ ll res=1; for(ll i=0;i<r;i++){res*=(n-i); res/=(i+1);} return res;}
-ll npr(ll n, ll r){ ll res=1; for(ll i=0;i<r;i++){res*=(n-i); } return res;}
+const int mod = 1e9+7;
+
 
 int main()
 {
@@ -34,6 +30,6 @@ int main()
     cin>>t;
     while(t--)
     {
-        
+        ll n; cin>>n;
     }  
 }
