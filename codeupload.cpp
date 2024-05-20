f20f18520243f38724e9d64de82895b19c5e00b7
Shikshak Kumar <138814848+CodeMaker46@users.noreply.github.com>
2024-05-20 14:24:02 +0530

    Update codeupload.cpp

--- codeupload.cpp
+++ codeupload.cpp
@@ -23,6 +23,12 @@
 using namespace std;
 const int mod = 1e9+7;
 
+ll gcd(ll a, ll b){if(b==0) return a;gcd(b,a%b);}
+ll prime(ll n){ if(n==1 || n==0) return 0; for(ll i=2;i*i<=n;i++) if(n%i==0) return 0;return 1;}
+ll power(ll x, ll n){ll res=1;while(n){if(n&1) res*=x,n--;else x*=x,n/=2;}return res;}
+ll ncr(ll n, ll r){ ll res=1; for(ll i=0;i<r;i++){res*=(n-i); res/=(i+1);} return res;}
+ll npr(ll n, ll r){ ll res=1; for(ll i=0;i<r;i++){res*=(n-i); } return res;}
+
 void solve()
 {
     ll n; cin>>n;
