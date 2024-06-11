a4ad3c0f3f66c8e68334f420668e165fa9852bd8
Shikshak Kumar <138814848+CodeMaker46@users.noreply.github.com>
2024-06-12 00:22:38 +0530

    Update c++_notes.cpp

--- c++_notes.cpp
+++ c++_notes.cpp
@@ -1,30 +1,38 @@
-// Formula : a*b = lcm(a,b) * gcd(a,b)
-// Formula to make pairs : (n*(n-1) )/2
-// Always use map for hashing instead of unordered_map as it may gives TLE
 
-// upper_bound(v.begin(),v.end(),U)
-// Returns an iterator pointing to the first element that is greater than U.
-// If no such element is found, returns last
+Formula : a*b = lcm(a,b) * gcd(a,b)
+Formula to make pairs : (n*(n-1) )/2
 
-// lower_bound(v.begin(),v.end(),L) gives you the pointer of the value greater than or equal to L
-// lower_bound(v.begin(),v.end(),L) - v.begin() gives you the index of the value greater than or equal to L
+Always use map for hashing instead of unordered_map as it may gives TLE
 
-// sort(v.begin(),v.end(),greater<ll>()); sorts the array in descending order
+INBUILT FUNCTIONS TO BE USED FOR EFFICIENT CODE :-
+  
+1.  upper_bound(v.begin(),v.end(),U);
+    Returns an iterator pointing to the first element that is greater than U.
+    If no such element is found, returns last
 
-// vec.insert(vec.begin(), x); to insert 'x' at the front of a vector 
+2.  lower_bound(v.begin(),v.end(),L); gives you the pointer of the value greater than or equal to L
+    lower_bound(v.begin(),v.end(),L) - v.begin(); gives you the index of the value greater than or equal to L
 
-// INT_MAX:INT_MAX is typically 2147483647.
-// INT_MAX is for int (usually 32-bit signed integer).
+3.  sort(v.begin(),v.end(),greater<ll>()); sorts the array in descending order
 
-// INT_MIN : -2^31
-// INT_MAX : 2^31 - 1
+4.  v.insert(v.begin(), x); to insert 'x' at the front of a vector 
 
-// LLONG_MAX: LLONG_MAX is typically 9223372036854775807.
-// LLONG_MAX is for long long (usually 64-bit signed integer).
+5.  isalpha('x'); checks x is char or not 
 
-// SLIDING WINDOW OR TWO POINTER CONCEPT
-// ll i=0,j=0;
-// while(j<n)
-// {
-//     while(condition not true) i++;
-// }
+INT_MAX:INT_MAX is typically 2147483647.
+INT_MAX is for int (usually 32-bit signed integer).
+
+INT_MIN : -2^31
+INT_MAX : 2^31 - 1
+
+LLONG_MAX: LLONG_MAX is typically 9223372036854775807.
+LLONG_MAX is for long long (usually 64-bit signed integer).
+
+SLIDING WINDOW OR TWO POINTER CONCEPT
+ll i=0,j=0;
+while(j<n)
+{
+    while(condition not true) i++;
+}
+
+
