99343eefa2c96883fcc1a7eccc8a917a12629d10
Shikshak Kumar <138814848+CodeMaker46@users.noreply.github.com>
2024-10-05 16:15:25 +0530

    Update codeupload.cpp

--- codeupload.cpp
+++ codeupload.cpp
@@ -76,78 +76,77 @@ ll GetHash(string s)
 
 // DSU code structure 
 class DisjointSet{
-	    public:
-	        vector<int> rank,size,parent;
-	        
-	        DisjointSet(int n)
-	        {
-	            rank.resize(n+1,0);
-	            parent.resize(n+1);
-	            
-	            for(int i=0;i<n;i++)
-	            {
-	                parent[i]=i;
-	            }
-	        }
-	        
-	        int findParent(int node)
-	        {
-	            if(parent[node]==node)
-	            {
-	                return node;
-	            }
-	            
-	            return parent[node] = findParent(parent[node]);
-	            
-	        }
-	        
-	        void unionByRank(int u, int v)
-	        {
-	            int u_parent = findParent(u);
-	            int v_parent = findParent(v);
-	            
-	            if(u_parent==v_parent) return ;
-	            
-	            if(rank[u_parent]<rank[v_parent])
-	            {
-	                parent[u_parent] = v_parent;
-	            }
-	            
-	            if(rank[u_parent]>rank[v_parent])
-	            {
-	                parent[v_parent] = u_parent;
-	            }
-	            
-	            else
-	            {
-	                parent[u_parent] = v_parent;
-	                rank[v_parent]++;
-	            }
-	            
-	        }
-	        
-	        void unionBySize(int u, int v)
-	        {
-	            int u_parent = findParent(u);
-	            int v_parent = findParent(v);
-	            
-	            if(u_parent==v_parent) return ;
-	            
-	            if(size[u_parent]<size[v_parent])
-	            {
-	                parent[u_parent] = v_parent;
-	                size[v_parent]+=size[u_parent];
-	            }
-	            
-	            else
-	            {
-	                parent[v_parent] = u_parent;
-	                size[u_parent]+=size[v_parent];
-	            }
-	            
-	        }
-	};
-
+    public:
+    vector<int> rank,size,parent;
+    
+    DisjointSet(int n)
+    {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+    }
+    
+    int findParent(int node)
+    {
+        if(parent[node]==node)
+        {
+            return node;
+        }
+        
+        return parent[node] = findParent(parent[node]);
+        
+    }
+    
+    void unionByRank(int u, int v)
+    {
+        int u_parent = findParent(u);
+        int v_parent = findParent(v);
+        
+        if(u_parent==v_parent) return ;
+        
+        if(rank[u_parent]<rank[v_parent])
+        {
+            parent[u_parent] = v_parent;
+        }
+        
+        if(rank[u_parent]>rank[v_parent])
+        {
+            parent[v_parent] = u_parent;
+        }
+        
+        else
+        {
+            parent[u_parent] = v_parent;
+            rank[v_parent]++;
+        }
+        
+    }
+    
+    void unionBySize(int u, int v)
+    {
+        int u_parent = findParent(u);
+        int v_parent = findParent(v);
+        
+        if(u_parent==v_parent) return ;
+        
+        if(size[u_parent]<size[v_parent])
+        {
+            parent[u_parent] = v_parent;
+            size[v_parent]+=size[u_parent];
+        }
+        
+        else
+        {
+            parent[v_parent] = u_parent;
+            size[u_parent]+=size[v_parent];
+        }
+        
+    }
+};
 // Constants
 const ll mod =  1e9+7;
 const lld pi= 3.141592653589793;
